972c9a60ca6609e5973429b89c0a647e27f1d467
ajmalgithubm <ajmalm76774@gmail.com>
2023-08-26 11:14:52 +0530

    loading icon added for table

--- client/src/components/Table/Table.jsx
+++ client/src/components/Table/Table.jsx
@@ -1,52 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Table.module.css';
 import axios from 'axios';
+import Loading from '../Loading/Loading'
 const Table = () => {
 
     const [monthList, setMonthList] = useState();
-    const month = ['January', 'February', 'March', 'April', 'May', "June", 'July', "August", "September", "October", "November", "December"]
+    const month = ['January', 'February', 'March', 'April', 'May', "June", 'July', "August", "September", "October", "November", "December"];
+    const getAllProduct = async (month = new Date().getMonth() + 1) => {
+        const userId = localStorage.getItem("userId");
+        const { data } = await axios.post("https://mess-marking-server.vercel.app/getAllMessList", {
+            month,
+            userId
+        })
+        setMonthList(data.messList)
+        console.log(data)
+    }
     useEffect(() => {
-        const getAllProduct = async () => {
-            const userId = localStorage.getItem("userId");
-            const month = new Date().getMonth() + 1;
-            const { data } = await axios.post("https://mess-marking-server.vercel.app/getAllMessList", {
-                month,
-                userId
-            })
-            setMonthList(data.messList)
-            console.log(data)
-        }
         getAllProduct()
     }, [])
     return (
         <div>
-            <div class={styles.container}>
-                <h3 className={styles.h3}>{monthList && month[monthList[0].month -1]} Marking</h3>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Date</th>
-                            <th>Breakfast</th>
-                            <th>Lunch</th>
-                            <th>Supper</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            monthList?.map((item, index) => {
-                                return(
-                                    <tr key={index}>
-                                        <td>{item.day}</td>
-                                        <td className={item.mark.B ? styles.done : styles.nope}> {item.mark.B ? "Done" : "Nope"}</td>
-                                        <td className={item.mark.L ? styles.done : styles.nope}> {item.mark.B ? "Done" : "Nope"}</td>
-                                        <td className={item.mark.S ? styles.done : styles.nope}> {item.mark.B ? "Done" : "Nope"}</td>
-                                    </tr>
-                                )
-                            })
-                        }
-                    </tbody>
-                </table>
-            </div>
+            {
+                monthList ? (<div class={styles.container}>
+                    <h3 className={styles.h3}>{monthList && month[monthList[0].month - 1]} Marking</h3>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Date</th>
+                                <th>Breakfast</th>
+                                <th>Lunch</th>
+                                <th>Supper</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                monthList?.map((item, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td>{item.day}</td>
+                                            <td className={item.mark.B ? styles.done : styles.nope}> {item.mark.B ? "Done" : "Nope"}</td>
+                                            <td className={item.mark.L ? styles.done : styles.nope}> {item.mark.B ? "Done" : "Nope"}</td>
+                                            <td className={item.mark.S ? styles.done : styles.nope}> {item.mark.B ? "Done" : "Nope"}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>) : <Loading/>
+            }
         </div>
     )
 }
